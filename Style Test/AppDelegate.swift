3a005034d4eaeab53999c0e8f515d8308da7342a
Denis Bystruev <github@learnSwift.ru>
2019-07-01 17:15:09 +0300

    Initial Commit

---
+++ Style Test/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Style Test
+//
+//  Created by Denis Bystruev on 01/07/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
